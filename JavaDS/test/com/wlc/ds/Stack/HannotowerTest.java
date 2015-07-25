1f3f92cdf33fc5f868e125e40e47eb3b815b9e57
lanchun <lanchun10@126.com>
2015-07-25 18:13:44 +0800

    汉诺塔
    采用栈实现

---
+++ JavaDS/test/com/wlc/ds/Stack/HannotowerTest.java
@@ -0,0 +1,28 @@
+package com.wlc.ds.Stack;
+
+import org.junit.Test;
+
+public class HannotowerTest {
+
+	@Test
+	public void test() {
+		SimpleStack<Integer> s1 = new SimpleStack<Integer>(3);
+		SimpleStack<Integer> s2 = new SimpleStack<Integer>(3);
+		SimpleStack<Integer> s3 = new SimpleStack<Integer>(3);
+		
+		Hannotower a = new Hannotower(s1,"A");
+		Hannotower b = new Hannotower(s2,"B");
+		Hannotower c = new Hannotower(s3,"C");
+
+		for (int i = 3; i > 0; i--) {
+			a.disks.push(i);
+		}
+		
+		System.out.println(a.disks);
+		
+		a.moveDisksTo(3, c, b);
+		
+		System.out.println(c.disks);
+	}
+
+}
